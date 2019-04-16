ab54cf7222ddfaaf46d05f16d2f7996291a38d0c
yharsh <Harsh@0098>
2019-04-16 15:56:02 +0530

    Add testcases

--- src/test/java/io/github/yharsh/login/helper/UserHelperTest.java
+++ src/test/java/io/github/yharsh/login/helper/UserHelperTest.java
@@ -1,31 +1,42 @@
 package io.github.yharsh.login.helper;
 
-import io.github.yharsh.login.captcha.CaptchaService;
 import io.github.yharsh.login.domain.User;
+import io.github.yharsh.login.domain.UserRepository;
 import io.github.yharsh.login.exception.ReCaptchaInvalidException;
 import io.github.yharsh.login.exception.ReCaptchaUnavailableException;
 import io.github.yharsh.login.exception.UserAlreadyExistsException;
 import io.github.yharsh.login.exception.UserNotFoundException;
+import io.github.yharsh.login.validation.InputValidator;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.regex.Matcher;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class UserHelperTest {
+    @MockBean
+    private InputValidator inputValidator;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @TestConfiguration
+    static class UserHelperTestConfig {
+        @Bean
+        public UserHelper userHelper() {
+            return new UserHelper();
+        }
+    }
+
     @Autowired
     private UserHelper userHelper;
-    @MockBean
-    private CaptchaService captchaService;
 
     @Test
     public void createUserWithValidInput() throws Exception {
@@ -35,27 +46,40 @@ public class UserHelperTest {
         user.setEmailId(emailId);
         user.setPassword("Password@123");
         user.setName(name);
-        Mockito.doNothing().when(captchaService).verify(Matchers.anyString());
+        Mockito.doNothing().when(inputValidator).validate(user);
+        Mockito.when(userRepository.insert(user)).thenReturn(user);
+
         userHelper.create(user);
 
-        User createdUser = userHelper.get(emailId);
-        Assert.assertEquals(emailId, createdUser.getEmailId());
-        Assert.assertEquals(name, createdUser.getName());
+        Mockito.verify(userRepository, Mockito.times(1)).insert(user);
     }
 
     @Test(expected = UserAlreadyExistsException.class)
     public void createShouldThrowExceptionIfUserAlreadyExist() throws Exception {
-        String emailId = "existing@domain.com";
-        String password = "Password@123";
-        Mockito.doNothing().when(captchaService).verify(Matchers.anyString());
-        userHelper.create(new User(emailId, password));
-        //Try to create same user
-        userHelper.create(new User(emailId, password));
+        User user = new User();
+        Mockito.doNothing().when(inputValidator).validate(user);
+        Mockito.doThrow(new DuplicateKeyException("Unique key voilated")).when(userRepository).insert(user);
+
+        userHelper.create(user);
     }
 
     @Test(expected = UserNotFoundException.class)
-    public void testForNonExistingUser() throws UserNotFoundException, ReCaptchaUnavailableException, ReCaptchaInvalidException {
-        Mockito.doNothing().when(captchaService).verify(Matchers.anyString());
-        userHelper.get("nonexisting@email.id");
+    public void getShouldThrowUserNotFoundExceptionWhenUserDoesNotExists() throws UserNotFoundException, ReCaptchaUnavailableException, ReCaptchaInvalidException {
+        String emailId = "nonexistent@domain.com";
+        Mockito.doReturn(null).when(userRepository).findFirstByEmailId(emailId);
+
+        userHelper.get(emailId);
+    }
+
+    @Test
+    public void getShouldReturnUserWhenUserExists() throws UserNotFoundException, ReCaptchaUnavailableException, ReCaptchaInvalidException {
+        String emailId = "nonexistent@domain.com";
+        User user = new User();
+        user.setEmailId(emailId);
+        Mockito.doReturn(user).when(userRepository).findFirstByEmailId(emailId);
+
+        User fetchedUser = userHelper.get(emailId);
+
+        Assert.assertEquals(user, fetchedUser);
     }
 }
