b49d10be5a18e6d8d9435b5c1269b80d133b22ed
yharsh <Harsh@0098>
2019-04-16 10:02:37 +0530

    Add client side validation

--- src/main/webapp/src/components/Register.js
+++ src/main/webapp/src/components/Register.js
@@ -8,10 +8,15 @@ import { ReCaptcha, loadReCaptcha } from 'react-recaptcha-google'
 export class Register extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            validationResult: {
+                valid: true
+            }
+        };
         this.register = this.register.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
         this.verifyCallback = this.verifyCallback.bind(this);
+        this.validate = this.validate.bind(this);
     }
 
     componentDidMount() {
@@ -50,28 +55,67 @@ export class Register extends React.Component {
     }
 
     register(e) {
-        this.setState({ ...this.state, error: null });
-        axios.post(USER_URL, this.state)
-            .then(res => {
-                this.setState({ ...this.state, created: true });
-            }).catch(e => {
-                this.errorReponseHandler(e);
-            });
+        const validationResult = this.validate();
+        console.log(validationResult);
+        if (validationResult.valid) {
+            this.setState({ ...this.state, error: null, validationResult });
+            axios.post(USER_URL, this.state)
+                .then(res => {
+                    this.setState({ ...this.state, created: true });
+                }).catch(e => {
+                    this.errorReponseHandler(e);
+                });
+        } else {
+            this.setState({ ...this.state, validationResult });
+        }
     }
 
     onInputChange(fieldName) {
         return (e) => this.setState({ ...this.state, [fieldName]: e.target.value });
     }
 
+    validate() {
+        const validationResult = {
+            valid: true
+        };
+        //Validate email id
+        const emailPattern = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/;
+        console.log(this.state);
+        if (!this.state.emailId || !this.state.emailId.match(emailPattern)) {
+            validationResult.valid = false;
+            validationResult.emailId = 'Invalid email id';
+        }
+        const passwordPattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20}$/;
+        if (!this.state.password || !this.state.password.match(passwordPattern)) {
+            validationResult.valid = false;
+            validationResult.passsword = 'Password should contain atleast one upppercase, one lowercase, one special(@, #, $, %), one numeric character and should be of length 6 to 20 characters';
+        }
+        //Indian format
+        const phoneNumberPattern = /^[0-9]{10}$/;
+        if (this.state.phoneNumber && this.state.phoneNumber.length > 0 && !this.state.phoneNumber.match(phoneNumberPattern)) {
+            validationResult.valid = false;
+            validationResult.phoneNumber = 'Invalid phone number';
+        }
+        const recaptchaPattern = /^[A-Za-z0-9_-]+$/;
+        if (!this.state.recaptchaToken || !this.state.recaptchaToken.match(recaptchaPattern)) {
+            validationResult.valid = false;
+            validationResult.recaptchaToken = 'Invalid captcha';
+        }
+        return validationResult;
+    }
+
     render() {
         return <div className='container app-form'>
             <h3>Regiser User</h3>
             <Alert msg={this.state.msg} />
-            {this.state.created && <div className='alert alert-success'>success: Click <Link to='/login'>here</Link> to login</div>}
+            {this.state.created && <div className='alert alert-success'>Success: click <Link to='/login'>here</Link> to login</div>}
             <input type='text' className='form-control' placeholder='Email id' onChange={this.onInputChange('emailId')} />
+            <Alert msg={this.state.validationResult.emailId} />
             <input type='password' className='form-control' placeholder='Password' onChange={this.onInputChange('password')} />
+            <Alert msg={this.state.validationResult.passsword} />
             <input type='text' className='form-control' placeholder='Name' onChange={this.onInputChange('name')} />
             <input type='text' className='form-control' placeholder='Phone number' onChange={this.onInputChange('phoneNumber')} />
+            <Alert msg={this.state.validationResult.phoneNumber} />
             <ReCaptcha
                 ref={(el) => { this.captchaDemo = el; }}
                 size="normal"
@@ -81,6 +125,7 @@ export class Register extends React.Component {
                 onloadCallback={this.onLoadRecaptcha}
                 verifyCallback={this.verifyCallback}
             />
+            <Alert msg={this.state.validationResult.recaptchaToken} />
             <button className='form-control btn btn-primary' onClick={this.register}>Register</button>
         </div>;
     }
